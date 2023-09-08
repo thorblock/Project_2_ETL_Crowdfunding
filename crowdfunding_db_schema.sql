42137d59cd58dd00203d38cb9dbb6138e68cb893
Thor Black <135666868+thorblock@users.noreply.github.com>
2023-09-07 19:19:41 -0700

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -8,26 +8,26 @@ CREATE TABLE category (
 );
 
 CREATE TABLE subcategory (
-    subcategory_id VARCHAR   NOT NULL,
-    subcategory VARCHAR   NOT NULL,
+    subcategory_id VARCHAR(10)   NOT NULL,
+    subcategory VARCHAR(100)  NOT NULL,
     PRIMARY KEY (subcategory_id)
 );
 
 CREATE TABLE campaign (
-    cf_id INTEGER   NOT NULL,
-    contact_id INTEGER   NOT NULL,
-    company_name VARCHAR   NOT NULL,
-    description VARCHAR   NOT NULL,
-    goal FLOAT   NOT NULL,
-    pledged FLOAT   NOT NULL,
-    outcome VARCHAR   NOT NULL,
-    backers_count INTEGER   NOT NULL,
-    country VARCHAR   NOT NULL,
-    currency VARCHAR   NOT NULL,
+    cf_id INTEGER(10)   NOT NULL,
+    contact_id INTEGER(10)   NOT NULL,
+    company_name VARCHAR(100)   NOT NULL,
+    description VARCHAR(100)   NOT NULL,
+    goal FLOAT(10)   NOT NULL,
+    pledged FLOAT(10)   NOT NULL,
+    outcome VARCHAR(10)   NOT NULL,
+    backers_count INTEGER(10)   NOT NULL,
+    country VARCHAR(10)   NOT NULL,
+    currency VARCHAR(10)   NOT NULL,
     launch_date DATE   NOT NULL,
     end_date DATE   NOT NULL,
-    category_id VARCHAR   NOT NULL,
-    subcategory_id VARCHAR   NOT NULL,
+    category_id VARCHAR(10)   NOT NULL,
+    subcategory_id VARCHAR(10)   NOT NULL,
     PRIMARY KEY (cf_id),
 	UNIQUE(contact_id),
 	FOREIGN KEY (category_id) REFERENCES category (category_id),
@@ -35,15 +35,15 @@ CREATE TABLE campaign (
 );
 
 CREATE TABLE contacts (
-    contact_id INTEGER   NOT NULL,
-    first_name VARCHAR   NOT NULL,
-    last_name VARCHAR   NOT NULL,
-    email VARCHAR   NOT NULL,
+    contact_id INTEGER(10)   NOT NULL,
+    first_name VARCHAR(100)   NOT NULL,
+    last_name VARCHAR(100)   NOT NULL,
+    email VARCHAR(100)   NOT NULL,
     PRIMARY KEY (contact_id),
 	FOREIGN KEY(contact_id) REFERENCES campaign (contact_id)
 );
 
-select * from contacts
-select * from category
-select * from subcategory
-select * from campaign
+select * from contacts;
+select * from category;
+select * from subcategory;
+select * from campaign;
